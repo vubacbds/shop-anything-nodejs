30e6cdca04a925ecf710ffb2d52bf5e54a9907e8
vubacbds <vubacbds@gmail.com>
2022-09-22 14:58:36 +0700

    oki r

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,13 +3,13 @@ const dotenv = require("dotenv");
 
 async function connect() {
   try {
-    await mongoose.connect(process.env.DB_URL, {
+    await mongoose.connect(process.env.DATABASE_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("Kết nối thành công !");
+    console.log("Kết nối thành công nhé !");
   } catch (error) {
-    console.log("Kết nối thất bại");
+    console.log("Kết nối thất bại rồi T_T");
   }
 }
 
