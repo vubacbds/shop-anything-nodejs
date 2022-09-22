048ff6e3273e099ffc0692ba8ff4bedc714b4305
vubacbds <vubacbds@gmail.com>
2022-09-22 15:04:26 +0700

    oki rrr

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 async function connect() {
   try {
-    await mongoose.connect(process.env.DATABASE_URL, {
+    await mongoose.connect(`${process.env.DATABASE_URL}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
